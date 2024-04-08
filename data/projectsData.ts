c3c1cf9cb3eed9fba360528a7b52cda963cb46c5
nataliagranato <natalia.granato@jackexperts.com>
2024-04-08 11:53:33 -0300

    feat: add new projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,47 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Kubecarga',
+    description: `Um exemplo de teste de carga em Go para clusters Kubernetes. O teste cria deployments de forma contínua em intervalos regulares para simular carga no cluster.`,
+    imgSrc: '/static/images/kubecarga.png',
+    href: 'https://github.com/Tech-Preta/kubecarga',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Helm Charts',
+    description: `Uma coleção de Helm Charts criados ou com contribuições de Natalia Granato para diferentes aplicações.`,
+    imgSrc: '/static/images/helm.png',
+    href: 'https://github.com/Tech-Preta/charts',
+  },
+  {
+    title: 'Packer Base',
+    description: `Uma base para a criação de imagens utilizando Packer, com provisionamento feito através de playbooks do Ansible.`,
+    imgSrc: '/static/images/packer.png',
+    href: 'https://github.com/Tech-Preta/packer_base',
+  },
+  {
+    title: 'Kubeshell',
+    description: `O projeto Kubeshell é uma coleção de scripts úteis e ferramentas de linha de comando para ajudar administradores de clusters Kubernetes.`,
+    imgSrc: '/static/images/kubeshell.png',
+    href: 'https://github.com/Tech-Preta/kubeshell',
+  },
+  {
+    title: 'Análise de Custos AWS com Python',
+    description: `Um script Python que permite ao usuário analisar os custos dos serviços AWS em um período de tempo específico.`,
+    imgSrc: '/static/images/aws-py.png',
+    href: 'https://github.com/Tech-Preta/aws-cost-py',
+  },
+  {
+    title: 'Kubesec',
+    description: `O projeto Kubesec é uma solução abrangente para análise e relatórios de segurança em clusters Kubernetes.`,
+    imgSrc: '/static/images/kubesec.png',
+    href: 'https://github.com/Tech-Preta/kubesec',
+  },
+  {
+    title: 'Gerador de Senhas com Redis',
+    description: `Projeto de dockerização de um gerador de senhas, com pipeline de build, push para o Docker Hub, scan de vulnerabilidades com o Trivy e assinatura de imagens com o Cosign. 
+    O gerador de senhas é uma aplicação em Python que utiliza o Redis para armazenar as senhas geradas.`,
+    imgSrc: '/static/images/dockerized.jpg',
+    href: 'https://github.com/Tech-Preta/giropops-senhas',
   },
 ]
 
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'Sempre aprendendo sobre Cloud Native, Kubernetes e Observability.',
   language: 'pt-br',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://nataliagranato.vercel.app',
+  siteRepo: 'https://github.com/nataliagranato/nataliagranato.github.io',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://bolha.us/@nataliagranato',
